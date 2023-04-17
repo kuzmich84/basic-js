240417f240036aad0b3b6df992f82356c48c4957
Dmitry Kuchukov <kudim@inbox.ru>
2023-04-17 11:55:00 +0300

    feat: implement task matrix recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,11 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class DepthCalculator {
+  calculateDepth(arr) {
+        let count = 1;
+        for (let i = 0; i < arr.length; i++) {
+          let j = 1;
+           if (Array.isArray(arr[i])) {
+            j = j + this.calculateDepth(arr[i]);
+          }
+          if (j > count) {
+            count = j;
+          }
+        }
+        return count;
+      }
 }
 
 module.exports = {
